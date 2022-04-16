f5337f5083042fffedd2453d3aa6d2c5befdc77c
massif <massif@DESKTOP-R5FGR5E>
2022-04-16 14:08:54 +0000

    Ajoutez des fichiers projet.

---
+++ Domin/Entity/SubCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domin.Entity
+{
+    public class SubCategory
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessageResourceType = typeof(Ressources.ResourceData), ErrorMessageResourceName = ("SubCategName"))]
+        [MaxLength(20, ErrorMessageResourceType = typeof(Ressources.ResourceData), ErrorMessageResourceName = ("MaxLength"))]
+        [MinLength(3, ErrorMessageResourceType = typeof(Ressources.ResourceData), ErrorMessageResourceName = ("MinLength"))]
+        public string Name { get; set; }
+
+        public Guid CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        public int CurrentStaut { get; set; }
+    }
+}
---
+++ Infrastructure/Data/FreeBookDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class FreeBookDbContext : IdentityDbContext
+    {
+        public FreeBookDbContext(DbContextOptions<FreeBookDbContext> options):base(options) 
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityUser>().ToTable("Users", "Identity");
+            builder.Entity<IdentityRole>().ToTable("Roles", "Identity");
+            //builder.Entity<IdentityUserLogin>().ToTable("UserLogin", "Identity");
+
+        }
+    }
+}
